2fa681146c0790f78c6b93ff08d5a23e70fbd113
MattM513 <146422602+MattM513@users.noreply.github.com>
2025-11-26 15:29:42 +0100

    Initial commit - Unity project

---
+++ Assets/InputTest.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputTest : MonoBehaviour
+{
+    public InputActionProperty testActionValue;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float value = testActionValue.action.ReadValue<float>();
+    }
+}
